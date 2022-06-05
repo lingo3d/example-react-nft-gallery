5eb43f074b35609467a4ec0d13de3ffdc1a7b10d
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-06-06 01:07:50 +0800

    updated to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from "react-dom/client"
 import './index.css'
 import App from './App'
+import React from "react"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root')!)
+root.render(<React.StrictMode><App /></React.StrictMode>)
